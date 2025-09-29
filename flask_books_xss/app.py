7aaa9d19299771ba646f5ad170866d8391d177fd
Aryan Aziz Mathiesen <aryanam@uia.no>
2025-09-29 17:28:10 +0200

    fix: uncomment

--- flask_books_xss/app.py
+++ flask_books_xss/app.py
@@ -7,12 +7,12 @@ from .storage import add_book, list_books
 load_dotenv()
 app = create_app()
 
-# if not app.config["VULNERABLE_MODE"]:
-#     Talisman(app, content_security_policy = {
-#         "default-src": "'self'",
-#         "script-src": "'self'",
-#         "style-src": "'self'"
-#     })
+if not app.config["VULNERABLE_MODE"]:
+    Talisman(app, content_security_policy = {
+        "default-src": "'self'",
+        "script-src": "'self'",
+        "style-src": "'self'"
+    })
     
 def main():
     app.run(debug=True)
